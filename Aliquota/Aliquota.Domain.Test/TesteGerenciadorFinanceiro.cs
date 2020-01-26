f2b0e63dbc324c71681d3276b0641e500742bda1
Vitor Filardi <rabelo51@gmail.com>
2020-01-26 11:25:48 -0300

    Add GerenciadorFinanceiro

---
+++ Aliquota/Aliquota.Domain.Test/TesteGerenciadorFinanceiro.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Aliquota.Domain.Test
+{
+	public class TesteGerenciadorFinanceiro
+	{
+		[Fact]
+		public void Resgatar_GerenciadorFinanceiro()
+		{
+			var opcoes = new DbContextOptionsBuilder<ContextoInvestimento>()
+				.UseInMemoryDatabase(databaseName: "Resgatar_GerenciadorFinanceiro")
+				.Options;
+
+			using (var contexto = new ContextoInvestimento(opcoes))
+			{
+				var servicoProduto = new ServicoProdutoFinanceiro(contexto);
+				servicoProduto.Adicionar("CDB");
+				servicoProduto.Adicionar("Poupança");
+				servicoProduto.Adicionar("TesouroDireto");
+
+				var servicoCliente = new ServicoCliente(contexto);
+				servicoCliente.Adicionar("João");
+				servicoCliente.Adicionar("Maria");
+				servicoCliente.Adicionar("José");
+
+				var servicoAplicacao = new ServicoAplicacao(contexto);
+				servicoAplicacao
+					.Adicionar(2500m, servicoCliente.BuscaPorNome("João"), servicoProduto.BuscaPorNome("CDB"));
+
+				servicoAplicacao
+					.Adicionar(2500m, servicoCliente.BuscaPorNome("Maria"), servicoProduto.BuscaPorNome("Poupança"));
+
+				servicoAplicacao
+					.Adicionar(2500m, servicoCliente.BuscaPorNome("José"), servicoProduto.BuscaPorNome("TesouroDireto"));
+
+			}
+
+			using (var contexto = new ContextoInvestimento(opcoes))
+			{
+				var servicoCliente = new ServicoCliente(contexto);
+				var servicoProduto = new ServicoProdutoFinanceiro(contexto);
+				var servicoAplicacao = new ServicoAplicacao(contexto);
+
+				Cliente clienteJoao = servicoCliente.BuscaPorNome("João");
+				ProdutoFinanceiro produtoCDB = servicoProduto.BuscaPorNome("CDB");
+
+				Cliente clienteMaria = servicoCliente.BuscaPorNome("Maria");
+				ProdutoFinanceiro produtoPoup = servicoProduto.BuscaPorNome("Poupança");
+
+				Cliente clienteJose = servicoCliente.BuscaPorNome("José");
+				ProdutoFinanceiro produtoTesouro = servicoProduto.BuscaPorNome("TesouroDireto");
+
+				Aplicacao aplicacaoJoao = servicoAplicacao.BuscaPorClienteProdutoFinanceiro(clienteJoao, produtoCDB);
+				Aplicacao aplicacaoMaria = servicoAplicacao.BuscaPorClienteProdutoFinanceiro(clienteMaria, produtoPoup);
+				Aplicacao aplicacaoJose = servicoAplicacao.BuscaPorClienteProdutoFinanceiro(clienteJose, produtoTesouro);
+
+				GerenciadorFinanceiro gerenciadorFinanceiro = 
+					new GerenciadorFinanceiro(servicoCliente, servicoProduto, servicoAplicacao);
+
+				// Cliente João, ProdutoFinanceiro CDB
+				Dictionary<string, decimal> resultado = gerenciadorFinanceiro.Resgatar(clienteJoao, produtoCDB, aplicacaoJoao.DataInicial.AddDays(365), 12m);
+
+				// 12% a.a -> 1,12 a.ano -> 1,0003105377556553767443007867263 a.dia
+				// valorBruto = 2500 * 1,0003105377556553767443007867263 ^ 365 dias = 2800 
+				// lucro = 300
+				// imposto = 300 * 0,225 = 67,5
+				// valorLiquido = 2800 - 67,5 = 2.732,5
+
+				Assert.Equal(2800.00m, Math.Round(resultado["valorBruto"], 2));
+				Assert.Equal(67.50m, Math.Round(resultado["imposto"], 2));
+				Assert.Equal(2732.50m, Math.Round(resultado["valorLiquido"], 2));
+				Assert.Equal(2, contexto.Aplicacoes.Count());
+
+				// Cliente Maria, ProdutoFinanceiro Poupança
+				Dictionary<string, decimal> resultadoMaria = gerenciadorFinanceiro.Resgatar(clienteMaria, produtoPoup, aplicacaoMaria.DataInicial.AddDays(500), 12m);
+
+				// 12% a.a -> 1,12 a.ano -> 1,0003105377556553767443007867263 a.dia (365 dias)
+				// valorBruto = 2500 * 1,0003105377556553767443007867263 ^ 500 dias = 2919,859522141612463743368052173 
+				// lucro = 419,85952214161246374336805217297‬
+				// imposto = lucro * 0,185 = 77,674011596198305792523089651999‬
+				// valorLiquido = 2919,859522141612463743368052173 - 77,674011596198305792523089651999 = 2.842,185510545414157950844962521
+				
+				Assert.Equal(2919.86m, Math.Round(resultadoMaria["valorBruto"], 2));
+				Assert.Equal(77.67m, Math.Round(resultadoMaria["imposto"], 2));
+				Assert.Equal(2842.19m, Math.Round(resultadoMaria["valorLiquido"], 2));
+				Assert.Equal(1, contexto.Aplicacoes.Count());
+
+				// Cliente José, ProdutoFinanceiro Tesouro Direto
+				Dictionary<string, decimal> resultadoJose = gerenciadorFinanceiro.Resgatar(clienteJose, produtoTesouro, aplicacaoJose.DataInicial.AddDays(800), 12m);
+
+				// 12% a.a -> 1,12 a.ano -> 1,0003105377556553767443007867263 a.dia
+				// valorBruto = 2500 * 1,0003105377556553767443007867263 ^ 800 dias = 3.204,904750515865147992845356895 
+				// lucro = 704,90475051586514799284535689503
+				// imposto = 704,90475051586514799284535689503 * 0,15 = 105,73571257737977219892680353425
+				// valorLiquido = 3.204,904750515865147992845356895 - 105,73571257737977219892680353425 = 3.099,1690379384853757939185533607
+
+				Assert.Equal(3204.90m, Math.Round(resultadoJose["valorBruto"], 2));
+				Assert.Equal(105.74m, Math.Round(resultadoJose["imposto"], 2));
+				Assert.Equal(3099.17m, Math.Round(resultadoJose["valorLiquido"], 2));
+				Assert.Equal(0, contexto.Aplicacoes.Count());
+
+				contexto.Database.EnsureDeleted();
+
+			}
+		}
+
+		[Fact]
+		public void Investir_GerenciadorFinanceiro()
+		{
+			var opcoes = new DbContextOptionsBuilder<ContextoInvestimento>()
+				.UseInMemoryDatabase(databaseName: "Investir_GerenciadorFinanceiro")
+				.Options;
+
+			using (var contexto = new ContextoInvestimento(opcoes))
+			{
+				var servicoProduto = new ServicoProdutoFinanceiro(contexto);
+				servicoProduto.Adicionar("CDB");
+
+				var servicoCliente = new ServicoCliente(contexto);
+				servicoCliente.Adicionar("João");
+
+				var servicoAplicacao = new ServicoAplicacao(contexto);
+
+				Cliente cliente = servicoCliente.BuscaPorNome("João");
+				ProdutoFinanceiro produto = servicoProduto.BuscaPorNome("CDB");
+
+				GerenciadorFinanceiro gerenciadorFinanceiro =
+					new GerenciadorFinanceiro(servicoCliente, servicoProduto, servicoAplicacao);
+
+				gerenciadorFinanceiro.Investir(cliente, produto, 2500.87m);
+
+				gerenciadorFinanceiro.Investir(cliente, produto, 1499.13m);
+
+			}
+
+			using (var contexto = new ContextoInvestimento(opcoes))
+			{
+				Assert.Equal(1, contexto.Aplicacoes.Count());
+				Assert.Equal(4000.00m, contexto.Aplicacoes.Single().Valor);
+
+				contexto.Database.EnsureDeleted();
+
+			}
+		}
+	}
+}
---
+++ Aliquota/Aliquota.Domain/GerenciadorFinanceiro.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aliquota.Domain
+{
+	public class GerenciadorFinanceiro
+	{
+		private readonly ServicoCliente _servicoCliente;
+		private readonly ServicoProdutoFinanceiro _servicoProdutoFinanceiro;
+		private readonly ServicoAplicacao _servicoAplicacao;
+
+		public GerenciadorFinanceiro(
+			ServicoCliente servicoCliente, 
+			ServicoProdutoFinanceiro servicoProdutoFinanceiro,
+			ServicoAplicacao servicoAplicacao)
+		{
+			_servicoCliente = servicoCliente;
+			_servicoProdutoFinanceiro = servicoProdutoFinanceiro;
+			_servicoAplicacao = servicoAplicacao;
+		}
+
+		/// <summary>
+		/// Realiza uma aplicação em um determinado produto financeiro para um cliente
+		/// Cria uma aplicação ou atualiza uma existente
+		/// </summary>
+		/// <param name="cliente">Cliente solicitante</param>
+		/// <param name="produto">Produto Financeiro escolhido</param>
+		/// <param name="valor">Valor da aplicação</param>
+		public void Investir(Cliente cliente, ProdutoFinanceiro produto, decimal valor)
+		{
+			Aplicacao aplicacao = _servicoAplicacao.BuscaPorClienteProdutoFinanceiro(cliente, produto);
+
+			if (aplicacao == null)
+				_servicoAplicacao.Adicionar(valor, cliente, produto);
+			else
+				_servicoAplicacao.Atualizar(valor, cliente, produto);
+		}
+
+		/// <summary>
+		/// Realiza a retirada de todo o dinheiro investido em uma aplicação
+		/// </summary>
+		/// <param name="cliente">Cliente solicitante</param>
+		/// <param name="produto">Produto Financeiro que contém a aplicação</param>
+		/// <param name="dataResgate">Data que será realizado o resgate</param>
+		/// <param name="rendimentoPercentualAno">
+		/// Taxa média percentual anual de rendimento da aplicação.
+		/// Exemplo: 12% a.a (ao ano)
+		/// </param>
+		/// <returns>
+		/// Um dicionário contendo o valorBruto, o imposto pago e o valorLiquido da aplicação
+		/// </returns>
+		public Dictionary<string, decimal> Resgatar(Cliente cliente, ProdutoFinanceiro produto, DateTime dataResgate, decimal rendimentoPercentualAno)
+		{
+			// Buscar a aplicação do cliente
+			Aplicacao aplicacao = _servicoAplicacao.BuscaPorClienteProdutoFinanceiro(cliente, produto);
+
+			if (aplicacao == null)
+				throw new Exception("A aplicação não foi encontrada.");
+
+			// obter o valor da aplicação
+			decimal valorTotalResgate = aplicacao.Valor;
+			DateTime dataAplicacao = aplicacao.DataInicial;
+
+			if (dataResgate < dataAplicacao)
+				throw new Exception("A data de resgate precisa ser após a data de aplicação");
+
+			TimeSpan duracaoAplicacao = dataResgate - dataAplicacao;
+			decimal lucro = CalculaLucro(valorTotalResgate, rendimentoPercentualAno, duracaoAplicacao);
+
+			// calcular imposto
+			decimal imposto = CalculaIR(lucro, duracaoAplicacao);
+
+			// construir resultado
+			Dictionary<string, decimal> resultado = new Dictionary<string, decimal>();
+			resultado.Add("valorBruto", valorTotalResgate + lucro);
+			resultado.Add("imposto", imposto);
+			resultado.Add("valorLiquido", valorTotalResgate + lucro - imposto);
+
+			// remover a aplicação
+			_servicoAplicacao.Remover(aplicacao);
+
+			return resultado; 
+		}
+
+		private decimal CalculaIR(decimal lucro, TimeSpan duracaoAplicacao)
+		{
+			if (lucro <= 0)
+				return 0;
+
+			if (duracaoAplicacao.Days <= 365)
+				return 0.225m * lucro;
+			else if (duracaoAplicacao.Days > 365 && duracaoAplicacao.Days <= 365 * 2)
+				return 0.185m * lucro;
+			else 
+				return 0.15m * lucro;
+		}
+
+		private decimal CalculaLucro(decimal valoBrutoResgate, decimal rendimentoPercentagemAoAno, TimeSpan duracao)
+		{
+			double rendimentoDecimal = (double)(rendimentoPercentagemAoAno / 100); // 112,5% -> 0,125
+			double fatorDiarioDeRendimento = Math.Pow(1 + rendimentoDecimal, 1f / 365f); // 1,125 ao ano -> 1,00032458 ao dia
+
+			double valorBrutoResgateComRendimento = Math.Pow(fatorDiarioDeRendimento, duracao.TotalDays) * (double)valoBrutoResgate;
+
+			return (decimal)valorBrutoResgateComRendimento - valoBrutoResgate;
+		}
+	}
+}
