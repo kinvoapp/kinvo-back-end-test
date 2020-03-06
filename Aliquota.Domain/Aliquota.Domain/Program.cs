679510a1b1a8f00e119fd184f060b25e2387da5a
lailacampos <enders.game1990@gmail.com>
2020-03-06 13:41:26 -0300

    Creating Project

---
+++ Aliquota.Domain/Aliquota.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aliquota.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
