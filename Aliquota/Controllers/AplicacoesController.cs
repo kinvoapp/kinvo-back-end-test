200779067ab5eec82d3ad5eca2a587151428d78f
Fellipe Capolupo <fellipe.capolupo@enecad.com.br>
2021-03-08 20:14:10 -0300

    Adicionados Controllers

---
+++ Aliquota/Controllers/AplicacoesController.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Aliquota.Domain.Entities;
+using Aliquota.Applications;
+
+namespace Aliquota.Domain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AplicacoesController : ControllerBase
+    {
+        private readonly IAppAplicacao _appAplicacao;
+
+        public AplicacoesController(IAppAplicacao appAplicacao)
+        {
+            _appAplicacao = appAplicacao;
+        }
+
+        // GET: api/Aplicacoes
+        [HttpGet]
+        public async Task<IActionResult> GetAplicacao()
+        {
+            return Ok(await _appAplicacao.Listar());
+        }
+
+        // GET: api/Aplicacoes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAplicacao(int id)
+        {
+            var cliente = await _appAplicacao.ObterPorId(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // PUT: api/Aplicacoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAplicacao(int id, Aplicacao aplicacao)
+        {
+            if (id != aplicacao.ID)
+            {
+                return BadRequest();
+            }
+
+            await _appAplicacao.Atualizar(aplicacao);
+
+            return Ok();
+        }
+
+        // POST: api/Aplicacoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<IActionResult> PostAplicacao(Aplicacao aplicacao)
+        {
+            await _appAplicacao.Adicionar(aplicacao);
+            return Ok();
+        }
+
+        // DELETE: api/Aplicacoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAplicacao(int id)
+        {
+            var aplicacao = await _appAplicacao.ObterPorId(id);
+            
+            if (aplicacao == null)
+                return NotFound();
+
+            await _appAplicacao.Excluir(aplicacao);
+            return Ok();
+        }        
+    }
+}
---
+++ Aliquota/Controllers/ProdutosFinanceirosController.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Aliquota.Domain.Entities;
+using Aliquota.Applications;
+
+namespace Aliquota.Domain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosFinanceirosController : ControllerBase
+    {
+        private readonly IAppProdutoFinanceiro _appProdutoFinanceiro;
+
+        public ProdutosFinanceirosController(IAppProdutoFinanceiro appProdutoFinanceiro)
+        {
+            _appProdutoFinanceiro = appProdutoFinanceiro;
+        }
+
+        // GET: api/ProdutosFinanceiros
+        [HttpGet]
+        public async Task<IActionResult> GetProdutosFinanceiros()
+        {
+            return Ok(await _appProdutoFinanceiro.Listar());
+        }
+
+        // GET: api/ProdutosFinanceiros/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProdutoFinanceiro(int id)
+        {
+            var ProdutoFinanceiro = await _appProdutoFinanceiro.ObterPorId(id);
+
+            if (ProdutoFinanceiro == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ProdutoFinanceiro);
+        }
+
+        // PUT: api/ProdutosFinanceiros/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProdutoFinanceiro(int id, ProdutoFinanceiro produtoFinanceiro)
+        {
+            if (id != produtoFinanceiro.ID)
+            {
+                return BadRequest();
+            }
+
+            await _appProdutoFinanceiro.Atualizar(produtoFinanceiro);
+
+            return Ok();
+        }
+
+        // POST: api/ProdutosFinanceiros
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<IActionResult> PostProdutoFinanceiro(ProdutoFinanceiro produtoFinanceiro)
+        {
+            await _appProdutoFinanceiro.Adicionar(produtoFinanceiro);
+            return Ok();
+        }
+
+        // DELETE: api/ProdutosFinanceiros/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProdutoFinanceiro(int id)
+        {
+            var ProdutoFinanceiro = await _appProdutoFinanceiro.ObterPorId(id);
+            
+            if (ProdutoFinanceiro == null)
+                return NotFound();
+
+            await _appProdutoFinanceiro.Excluir(ProdutoFinanceiro);
+            return Ok();
+        }        
+    }
+}
