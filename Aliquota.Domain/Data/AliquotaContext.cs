08e1a9df44219040b89c32571073c21c5ba39225
Ana Tartari <anatartari412@gmail.com>
2021-04-09 19:07:53 -0300

    Aliquota Context

---
+++ Aliquota.Domain/Data/AliquotaContext.cs
@@ -0,0 +1,19 @@
+﻿using Aliquota.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliquota.Domain.Data
+{
+    class AliquotaContext :DbContext
+    {
+        public DbSet<Aplicacao> Aplicacoes { get; set; }
+        public DbSet<Resgate> Resgates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite("Data Source=AliquotaDB.db");
+    }
+}
