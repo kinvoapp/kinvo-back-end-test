92fe04d511e4c7fc9f775d91255a6acc91ad3152
Ana Tartari <anatartari412@gmail.com>
2021-04-09 19:07:24 -0300

    Criação de Models

---
+++ Aliquota.Domain/Models/Aplicacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aliquota.Domain.Models
+{
+    public class Aplicacao
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public double Valor { get; set; }
+        public double Rentabilidade_Mes { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
---
+++ Aliquota.Domain/Models/Resgate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliquota.Domain.Models
+{
+    class Resgate
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Lucro { get; set; }
+        public double Valor_IR { get; set; }
+        public DateTime Data { get; set; }
+        public int AplicacaoId { get; set; }
+        public Aplicacao Aplicacao { get; set; }
+    }
+}
