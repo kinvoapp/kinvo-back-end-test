54eacc37a514a39519c3ad394ac67a9e5ee3fd50
titosilva <jt.mat@hotmail.com>
2021-07-11 15:13:56 -0300

    feat: added investment evaluators

--- Aliquota.Domain/Entities/FinancialProduct.cs
+++ Aliquota.Domain/Entities/FinancialProduct.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json;
+
 namespace Aliquota.Domain.Entities
 {
     public class FinancialProduct : EntityBase
@@ -5,5 +10,13 @@ namespace Aliquota.Domain.Entities
         public FinancialProduct() : base() { }
 
         public string Name { get; set; }
+
+        [NotMapped]
+        public List<InvestmentEvaluatorCommand> Evaluators { get; set; } // Dont store this
+
+        public string EvaluatorsJson { // Store this instead
+            get => JsonSerializer.Serialize(Evaluators);
+            set => JsonSerializer.Deserialize<List<InvestmentEvaluatorCommand>>(value);
+        }
     }
 }
