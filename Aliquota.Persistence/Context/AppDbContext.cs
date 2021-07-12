a3f52b980cfc0609075464d3c8460fbd6a2b3a3f
titosilva <jt.mat@hotmail.com>
2021-07-12 12:30:02 -0300

    feat: added and improved DataInitializers

--- Aliquota.Persistence/Context/AppDbContext.cs
+++ Aliquota.Persistence/Context/AppDbContext.cs
@@ -8,5 +8,6 @@ namespace Aliquota.Persistence.Context {
         public DbSet<User> Users { get; set; }
         public DbSet<Portfolio> Portfolios { get; set; }
         public DbSet<FinancialProduct> FinancialProducts { get; set; }
+        public DbSet<Investment> Investments { get; set; }
     }
 }
