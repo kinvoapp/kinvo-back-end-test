f149a85d7c42ad9ae55ab4ca4ef0d89214d16097
titosilva <jt.mat@hotmail.com>
2021-07-13 09:01:43 -0300

    feat: added investment listing

--- Aliquota.WebApp/ClientApp/src/app/components/investment/portfolio/portfolio.component.ts
+++ Aliquota.WebApp/ClientApp/src/app/components/investment/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { PortfolioModel } from 'src/app/api/models/portfolio-models';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
@@ -7,13 +9,29 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  loading: boolean = true;
+  portfolio: PortfolioModel;
 
   constructor(
     private portfolioService: PortfolioService,
+    private snackBar: MatSnackBar,
   ) { }
 
+  get totalInvested() : number {
+    return this.portfolio.investments.map(i => i.initialValue).reduce((acc, v) => acc + v);
+  }
+
   ngOnInit() {
-    this.portfolioService.getPortfolio().subscribe();
+    this.portfolioService.getPortfolio().subscribe(
+      res => {
+        this.portfolio = res;
+        this.loading = false;
+      },
+      err => {
+        this.snackBar.open("Ocorreu uma falha ao tentar obter o seu portfólio");
+        this.loading = false;
+      },
+    );
   }
 
 }
