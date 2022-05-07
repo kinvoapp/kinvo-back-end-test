770fb72499b51126de165677c7bb781286bec19e
César Murilo da Silva Júnior <cesar_vn2010@hotmail.com>
2022-05-07 08:18:09 -0300

    dev

--- Aliquota.Api/Program.cs
+++ Aliquota.Api/Program.cs
@@ -8,5 +8,110 @@ namespace Aliquota.Api
         {
             Console.WriteLine("Hello World!");
         }
+
+        static string LerOpcaoMenu()
+        {
+            string opcao;
+            Console.Write("Opção desejada: ");
+            opcao = Console.ReadLine();
+            return opcao;
+        }
+
+        static string MostrarMenu()
+        {
+            string menu = "Escolha uma opção:\n\n" +
+                          " 1 - Realizar aplicação\n" +
+                          " 2 - Realizar resgate\n" +;
+                          " 3 - Ver histórico de movimentaçõesz\n" +
+                          " 4 - Sair do Programa \n"; 
+            return menu;
+        }
+
+        statis string MostrarCabecalho()
+        {
+            return "[ ALIQUOTA ]\n";
+        }
+
+        static void ProcessarOpcaoMenu(sting opcao)
+        {
+            switch(opcao)
+            {
+                case "1":
+                    RealizarAplicacao();
+                    break;
+                case "2":
+                    RealizarResgate();
+                    break;
+                case "3":
+                    VerHistorico();
+                    break;
+                case "4":
+                    Console.WriteLine("Até mais!");
+                    break;
+                default:
+                    Console.WriteLine("Opção de menu inválida.");
+                    break;
+            }
+        }
+
+        private static void VerHistorico()
+        {
+            var comando = new MovimentacaoComando();
+            var movimentacoes = comando.ListarTodas();
+            
+            if (movimentacoes!=null)
+            {
+                foreach (var movimentacao in movimentacoes )
+                {
+                    Console.WriteLine("\n"+movimentacao.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("A consulta não retornou movimentações.");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void RealizarAplicacao()
+        {
+            var movimentacaoDTO = new MovimentacaoDTO();
+            var comando = new MovimentacaoComando();
+            char opcao;
+            Console.Clear();
+            Console.WriteLine("\n[APLICACAO]");
+            Console.Write("Valor: ");
+            movimentacaoDTO.Valor = Console.ReadLine();
+            movimentacaoDTO.DataMovimentacao = DateTime.Now;
+            movimentacaoDTO.Tipo = "Aplicacao";
+
+            if(movimentacaoComando.Adicionar(movimentacaoDTO))
+            {
+                Console.WriteLine("Aplicação Realizada com sucesso!");
+                Console.ReadKey();
+            }
+
+        }
+
+        private static void RealizarResgate()
+        {
+            var movimentacaoDTO = new MovimentacaoDTO();
+            var comando = new MovimentacaoComando();
+            char opcao;
+            Console.Clear();
+            Console.WriteLine("\n[Resgate]");
+            Console.Write("Valor: ");
+            movimentacaoDTO.Valor = Console.ReadLine();
+            movimentacaoDTO.DataMovimentacao = DateTime.Now;
+            movimentacaoDTO.Tipo = "Resgate";
+
+            if(movimentacaoComando.Adicionar(movimentacaoDTO))
+            {
+                Console.WriteLine("Aplicação Realizada com sucesso!");
+                Console.ReadKey();
+            }
+
+        }
     }
 }
