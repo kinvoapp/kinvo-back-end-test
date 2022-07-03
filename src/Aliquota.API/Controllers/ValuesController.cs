f47185c336ed4e266f069c6629e90ec37fa4069f
George Duarte <contato@georgeduarte.com.br>
2022-07-03 10:43:57 -0300

    Estrutura base do projeto

---
+++ src/Aliquota.API/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aliquota.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
